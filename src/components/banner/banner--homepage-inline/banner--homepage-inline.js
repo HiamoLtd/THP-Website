21c7dbde1a50142c1099aff574c8e5d903d81a9e
alexjhockley <alexjhockley@gmail.com>
2024-09-09 19:07:25 +1200

    Home set fillter text on banner

--- src/components/banner/banner--homepage-inline/banner--homepage-inline.js
+++ src/components/banner/banner--homepage-inline/banner--homepage-inline.js
@@ -25,7 +25,7 @@ const BannerHomepageInline = ({
                 <hr />
                 {/* Homepage content is never richtext */}
                 {/* <p className={styles.content}>{content}</p> */}
-                <p className={styles.content}>A one-liner about The Heritage Practice</p>
+                <p className={styles.content}>[A one-liner about The Heritage Practice]</p>
               </>
             )}
           </Container>
