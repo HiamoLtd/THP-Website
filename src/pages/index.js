34f41e7b859884861930d18f4dcd8bf0f275da95
alexjhockley <alexjhockley@gmail.com>
2024-09-09 18:28:27 +1200

    Add filler homepage modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,10 @@ import { graphql } from 'gatsby';
 import get from 'lodash/get';
 
 import { Seo } from '../components/shared';
-import { Layout, NewsletterSignup, Sponsors, Banner } from '../components';
+import { Layout, Banner } from '../components';
 
 class RootIndex extends React.Component {
   render() {
-    // const events = get(this, 'props.data.allContentfulEvent.nodes');
     const homepage = get(this, 'props.data.contentfulHomepage');
     const seo = homepage.seo;
 
@@ -15,7 +14,7 @@ class RootIndex extends React.Component {
       <Layout location={this.props.location}>
         <Seo
           title={seo?.title || homepage.title}
-          description={seo?.description || "Experience Wellington's people, places, and stories."}
+          description={seo?.description || "The Heritage Practice is here to help you."}
           imageUrl={seo?.img?.url || homepage.bannerImg?.img?.url}
           keywords={seo?.keywords}
           robots={seo?.allowRobots}
@@ -23,26 +22,53 @@ class RootIndex extends React.Component {
         {/* Image banner with text */}
         <Banner
           type="homepage"
-          title={homepage.title}
+          title={"The Heritage Practice"}
           content={homepage.bannerContent?.bannerContent}
           image={homepage.bannerImage}
         />
-        {/* "Information" banner about the state of the festival */}
-          {/* TODO: Hide if there are upcoming events? Not really needed if there are events to see */}
-          {/* TODO: Part of banner module, just a submodule or something */}
-          {/* TODO: Later, ditch this here, use a "Site Status" class instead so we don't need to update it all the time */}
+        {/* THP - Purpose Statement Section */}
         {homepage.ctaContent && (
           <Banner
             type="cta"
+            title="The Heritage Practice"
             content={homepage.ctaContent}
             usesRichtext={true}
           />
         )}
-        {/* Upcoming events section */}
-          {/* TODO: Need this to be rearranged by date which we can't do yet */}
-        {/* Newsletter signup */}
-        <NewsletterSignup />
-        {/* Contact Call To Action banner */}
+        {/* THP - Who We Are Section */}
+          {/* TODO: Brief into text, photo, link to about page */}
+        {homepage.ctaContent && (
+          <Banner
+            type="cta"
+            title="Who We Are"
+            content={homepage.ctaContent}
+            usesRichtext={true}
+          />
+        )}
+        
+        {/* THP - More About Us Section */}
+        {/* TODO: Same section type as above, but with different content somehow */}
+        {homepage.ctaContent && (
+          <Banner
+            type="cta"
+            title="More About Us"
+            content={homepage.ctaContent}
+            usesRichtext={true}
+          />
+        )}
+        
+        {/* THP - Services We Provide Section */}
+        {/* TODO: Three sections, title, description, link for each one? Or just link to services page? */}
+        {homepage.ctaContent && (
+          <Banner
+            type="cta"
+            title="What We Do"
+            content={homepage.ctaContent}
+            usesRichtext={true}
+          />
+        )}
+
+        {/* THP - Contact Section */}
         {homepage.contactContent && (
           <Banner
             type="cta"
@@ -52,24 +78,22 @@ class RootIndex extends React.Component {
             usesRichtext={true}
           />
         )}
+
+        {/* Newsletter signup */}
+        {/* <NewsletterSignup /> */}
+        {/* Contact Call To Action banner */}
+        {/* {homepage.contactContent && (
+          <Banner
+            type="cta"
+            title="Contact Us"
+            content={homepage.contactContent}
+            usesRichtext={true}
+          />
+        )} */}
         {/* Sponsors */}
-        <Sponsors />
+        {/* <Sponsors /> */}
 
         {/* TODO: */}
-        {/* Homepage needs:
-          - Banner heading
-          - Banner content
-          - Banner image
-          - CTA content - From site state? Try later
-          - CTA image - From site state? Try later
-          - Contact heading
-          - Contact content
-          - SEO
-          - Newsletter
-
-          From site settings comes:
-          - Sponsors
-        */}
       </Layout>
     )
   }
