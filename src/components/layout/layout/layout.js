740705df2a467f0669998bbb345e7f1125342ba1
alexjhockley <alexjhockley@gmail.com>
2025-01-27 16:36:29 +1300

    Style - Add new poppins font for headers

--- src/components/layout/layout/layout.js
+++ src/components/layout/layout/layout.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Script } from "gatsby";
-
+import { Helmet } from 'react-helmet';
 import '../../../styles/variables.css';
 import '../../../styles/global.css';
-import { Seo } from '../../shared';
 import Navigation from '../navbar';
 import Footer from '../footer';
+
+// Add any necessary tags to the header, like imported fonts.
+const SiteHead = () => (
+  <Helmet>
+    <meta name="nonsense" />
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'/>
+  </Helmet>
+);
 
 // Setup and attach Google Analytics script using Gatsby
 // Script module & template literals to wrap the JS
@@ -39,6 +46,7 @@ class Layout extends React.Component {
 
     return (
       <>
+        <SiteHead />
         <GoogleAnalytics />
         <Navigation />
         <main style={{
